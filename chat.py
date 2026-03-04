5396e97211285d84d4360eaa643c1202b27ab352
RafaelMachado914 <lafaellascado2004@gmail.com>
2026-03-04 08:31:09 -0300

    implementa funcionalidades de entrada e saída de mensagens, faz a troca de multiplas threads para múltiplos processos, ajusta a interface do usuário, passa a logica para a interface

--- chat.py
+++ chat.py
@@ -1,49 +1,34 @@
 import customtkinter as ctk
 import rede
+import json
+from datetime import datetime
 
 
 
 ctk.set_appearance_mode("dark")
 chat = ctk.CTk()
-chat.title("Chat")
 chat.geometry("900x600")
 chat.grid_columnconfigure(0, weight=1) 
 chat.grid_columnconfigure(1, weight=3)
 chat.grid_rowconfigure(0, weight=1)
-chat.grid_rowconfigure(1, weight=0) 
-
 lateral = ctk.CTkFrame(chat, corner_radius=0)
 lateral.grid(row=0, column=0, sticky="nsew")
 
 
-
 LabelUsusario = ctk.CTkLabel(lateral, text="Usuário")
 LabelUsusario.pack(pady=10)
-
 Entrada_usuario = ctk.CTkEntry(lateral, placeholder_text="Usuario")
 Entrada_usuario.pack(pady=5)
 
-
 IP_Conversa = ctk.CTkLabel(lateral, text="IP da conversa")
 IP_Conversa.pack(pady=5)
-
 Entrada_ip = ctk.CTkEntry(lateral, placeholder_text="IP")
 Entrada_ip.pack(pady=5)
 
 Porta = ctk.CTkLabel(lateral, text="Selecione a porta")
 Porta.pack(pady=5)
-
 Porta_selecionada = ctk.CTkEntry(lateral, placeholder_text="Porta")
 Porta_selecionada.pack(pady=5)
-
-
-botao_entrar = ctk.CTkButton(lateral, text="Entrar",command=entrar)
-botao_entrar.pack(pady=20)
-
-def entrar():
-    rede.usuario = Entrada_usuario.get()
-
-
 
 Texto_chat = ctk.CTkTextbox(chat, width=500)
 Texto_chat.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
@@ -53,29 +38,62 @@ Texto_chat.configure(state="disabled")
 
 Caixa_entrada = ctk.CTkFrame(chat, fg_color="transparent")
 Caixa_entrada.grid(row=1, column=1, sticky="ew", padx=10, pady=10)
-
-
 Caixa_entrada.grid_columnconfigure(0, weight=1)
 Caixa_entrada.grid_columnconfigure(1, weight=0)
-
 entrada_mensagem = ctk.CTkEntry(Caixa_entrada, placeholder_text="Digite...") 
 entrada_mensagem.grid(row=0, column=0, sticky="ew", padx=(0, 10))
 
-botao_enviar = ctk.CTkButton(Caixa_entrada, text="Enviar", width=80, command=enviar)
-botao_enviar.grid(row=0, column=1)
 
 def enviar():
     texto = entrada_mensagem.get()
+    rede.ip_grupo = Entrada_ip.get()
+    rede.porta = int(Porta_selecionada.get())
     rede.servidor_udp.sendto(
         json.dumps({
             "date": datetime.now().strftime("%d/%m/%Y"),
             "time": datetime.now().strftime("%H:%M:%S"),
             "username": rede.usuario,
             "message": texto
-        }).encode("utf-8"),
-        (rede.ip_grupo, rede.porta)
-    )
+        }).encode("utf-8"),(rede.ip_grupo, rede.porta) )
     entrada_mensagem.delete(0, "end")
+
+
+
+def mostrar_mensagem(msg):
+    Texto_chat.configure(state="normal")
+
+    Texto_chat.insert(
+        "end",
+        f"\n{msg['date']} {msg['time']}\n{msg['username']}: {msg['message']}\n"
+    )
+
+    Texto_chat.configure(state="disabled")
+    Texto_chat.see("end")
+
+
+
+def entrar():
+    rede.usuario = Entrada_usuario.get()
+    rede.recebida = mostrar_mensagem
+    rede.iniciar(Entrada_ip.get(), int(Porta_selecionada.get()))
+
+botao_entrar = ctk.CTkButton(lateral, text="Entrar",command=entrar)
+botao_entrar.pack(pady=20)
+
+def sair():
+    rede.ip_grupo = None
+    rede.porta = None
+    rede.servidor_udp.close()
+    chat.destroy()
+
+botao_sair = ctk.CTkButton(lateral, text="Sair",command=sair)
+botao_sair.pack(pady=20)
+
+
+#nao ta funcionando KKKKKKKKKKKKKKKKKKKKKKKKK coringuei 
+
+botao_enviar = ctk.CTkButton(Caixa_entrada, text="Enviar", width=80, command=enviar)
+botao_enviar.grid(row=0, column=1)
 
 
 
--- rede.py
+++ rede.py
@@ -1,44 +1,43 @@
 import socket
 import struct
-import threading
+import multiprocessing
 import json
 from datetime import datetime
 
-ip_grupo = '224.1.1.1'
-porta = 5000
+ip_grupo = None
+porta = None
+servidor_udp = None
+usuario = None
+recebida = None
 
-servidor_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-try:
+def iniciar(ip, port):
+    global ip_grupo, porta, servidor_udp, usuario, recebida
+    ip_grupo = ip
+    porta = port
+    servidor_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     servidor_udp.bind(('', porta))
 
-except:
-    print("Erro, tente novamente")
+    enpacotar = struct.pack("4s4s", socket.inet_aton(ip_grupo), socket.INADDR_ANY,socket.inet_aton("0.0.0.0"))
+    servidor_udp.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, enpacotar)
 
-
-enpacotar = struct.pack("4sl", socket.inet_aton(ip_grupo), socket.INADDR_ANY)
-servidor_udp.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, enpacotar)
-
-usuario = 'usuario'
-
-recebida = ''
+    Recebendo = multiprocessing.Process(target=receber_mensagens,args=(servidor_udp,),daemon=True)
+    Recebendo.start()
 
 def receber_mensagens(servidor_udp):
    while True:
        try:
-           dados, mensagem = servidor_udp.recv(2048)
-           mensagem_convertida = json.loads(dados.decode('utf-8'))
-           recebida = mensagem_convertida
-           return recebida
+
+           data, addr = servidor_udp.recvfrom(2048)
+           mensagem_convertida = json.loads(data.decode('utf-8'))
+
+
+           if recebida:
+                recebida(mensagem_convertida)
        except:
            servidor_udp.close()
            break
+       
 
 
 
-Recebendo = threading.Thread(target=receber_mensagens, args=[servidor_udp])
-Enviando = threading.Thread(target=enviar_mensagens, args=[servidor_udp,usuario])
-
-
-Recebendo.start()
-Enviando.start()
